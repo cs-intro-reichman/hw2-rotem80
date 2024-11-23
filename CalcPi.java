b9af22bf8b5a3ed360223ba2c9a4b5fb5b2e9e7d
rotem80 <rrolev18@gmail.com>
2024-11-23 09:05:38 -0800

    check 3

--- CalcPi.java
+++ CalcPi.java
@@ -1,19 +1,32 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-		int terms = Integer.parseInt(args[0]);  
+    public static void main(String[] args) {
+       
+        if (args.length == 0) {
+            System.out.println("נא לספק מספר מונחים כקלט.");
+            return;
+        }
+
+       
+        int terms = Integer.parseInt(args[0]);
+
+       
         double piApprox = 0.0;
+
+       
         for (int i = 0; i < terms; i++) {
             double term = 1.0 / (2 * i + 1);
             if (i % 2 == 1) {
                 piApprox -= term; 
             } else {
-                piApprox += term;  
+                piApprox += term; 
             }
         }
 
-        piApprox *= 4; 
+       
+        piApprox *= 4;
+
+      
         System.out.println("pi according to Java: " + Math.PI);
-        System.out.println("pi, approximated: " + piApprox);
+        System.out.printf("pi, approximated: %.15f\n", piApprox); // הצגה עם דיוק של 15 ספרות
     }
 }
--- Cheers.java
+++ Cheers.java
@@ -3,15 +3,21 @@ public class Cheers {
         public static void main(String[] args) {
                 String input = args[0].toUpperCase();
                 int repetitions = Integer.parseInt(args[1]);
-                String vowels = "AEFHI LMNORSX";
                 for (int i = 0; i < input.length(); i++) {
                     char ch = input.charAt(i);
-                    String article = (vowels.indexOf(ch) >= 0) ? "an" : "a";  // "an" if vowel
+                    String article = (isVowel(ch)) ? "an" : "a"; 
                     System.out.println("Give me " + article + " " + ch + ": " + ch + "!");
                 }
+        
+                System.out.println("What does that spell?");
                 for (int i = 0; i < repetitions; i++) {
                     System.out.println(input + "!!!");
                 }
+            }
+        
+            private static boolean isVowel(char ch) {
+                String vowels = "AEIOU";
+                return vowels.indexOf(ch) >= 0;
             }
         }
         
