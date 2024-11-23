04efa364b57d4e6f6aff3782455e64b1e6c460ae
rotem80 <rrolev18@gmail.com>
2024-11-23 14:03:38 -0800

    chhers2

--- Cheers.java
+++ Cheers.java
@@ -3,23 +3,21 @@ public class Cheers {
         String word = args[0];
         int repeatCount = Integer.parseInt(args[1]);
         String upperWord = word.toUpperCase();
-        String specialChars = "AEFHILMNORSX";
+        String vowels = "AEIOU";
 
         for (int i = 0; i < upperWord.length(); i++) {
             char letter = upperWord.charAt(i);
-            if (specialChars.indexOf(letter) == -1) {
-                System.out.println("Give me a " + letter + ": " + letter + "!");
-            } else {
+            if (vowels.indexOf(letter) != -1) {
                 System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
             }
         }
 
-        int counter = 0;
         System.out.println("What does that spell?");
 
-        while (counter < repeatCount) {
+        for (int i = 0; i < repeatCount; i++) {
             System.out.println(upperWord + "!!!");
-            counter++;
         }
     }
 }
