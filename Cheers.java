8a265ffb76e5be570b721b68c08fc48e6a6b64e5
rotem80 <rrolev18@gmail.com>
2024-11-23 16:46:23 -0800

    27

--- Cheers.java
+++ Cheers.java
@@ -1,34 +1,31 @@
 public class Cheers {
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Usage: java Cheers <name> <num>");
+            System.out.println("Usage: java Cheers <word> <count>");
             return;
         }
 
-        String name = args[0].toUpperCase();
-        int num = Integer.parseInt(args[1]);
+        String word = args[0].toUpperCase();
+        int count = Integer.parseInt(args[1]);
 
-        String vowelsAndSpecial = "AEFHILMNORSX";
+        String vowels = "AEFHILMNORSX";
 
-        for (int i = 0; i < name.length(); i++) {
-            char ch = name.charAt(i);
-
-            if (vowelsAndSpecial.indexOf(ch) != -1) {
-                System.out.println("Give me an " + ch + ": " + ch + "!");
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (vowels.indexOf(letter) != -1) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
             } else {
-                System.out.println("Give me a " + ch + ": " + ch + "!");
+                System.out.println("Give me a " + letter + ": " + letter + "!");
             }
         }
 
         System.out.println("What does that spell?");
 
-        for (int i = 0; i < num; i++) {
-            if (i > 0) {
-                System.out.print(" "); 
-            }
-            System.out.print(name + "!!!");
+        StringBuilder cheers = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            cheers.append(word).append(" ");
         }
 
-        System.out.println();
+        System.out.println(cheers.toString().trim() + "!!!");
     }
 }
